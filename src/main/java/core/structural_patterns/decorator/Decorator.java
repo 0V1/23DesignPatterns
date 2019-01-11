5b011ccb85b136aa8cba45f23b801ca25ed977b1
qinfeng <qinfeng@zhuoyuan-info.com>
2019-01-11 18:06:18 +0800

    ~

--- src/main/java/core/structural_patterns/decorator/Decorator.java
+++ src/main/java/core/structural_patterns/decorator/Decorator.java
@@ -6,7 +6,7 @@ package core.structural_patterns.decorator;
  * @Description: 装饰模式：
  * 顾名思义，装饰模式就是给一个对象增加一些新的功能，
  * 而且是动态的，要求装饰对象和被装饰对象实现同一个接口，
- * 装饰对象持有被装饰对象的实例，sss
+ * 装饰对象持有被装饰对象的实例，sssasdsad
  * @author qinf QQ:908247035
  * @date 2017年2月15日
  * @version V1.0

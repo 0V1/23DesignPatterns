4e8853eee1cb49c4f77c7a8d835dc052b17eba2d
Hermes <908247035@qq.com>
2019-07-28 22:07:18 +0800

    update decorator patterms

--- src/main/java/patterns/structural/decorator/Decorator.java
+++ src/main/java/patterns/structural/decorator/Decorator.java
@@ -6,9 +6,7 @@ package patterns.structural.decorator;
  * @Description: 装饰模式：
  * 顾名思义，装饰模式就是给一个对象增加一些新的功能，
  * 而且是动态的，要求装饰对象和被装饰对象实现同一个接口，
- * 装饰对象持有被装饰对象的实例，sssasdsad
- * @author qinf QQ:908247035
- * @date 2017年2月15日
+ * 装饰对象持有被装饰对象的实例，
  * @version V1.0
  */
 public class Decorator implements Sourceable{
